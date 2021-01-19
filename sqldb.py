e42f9bb546c66f0fc5028f1f016be2fcc227be06
lushan1980 <shanswork@yahoo.com>
2021-01-19 15:50:56 -0500

    update

--- sqldb.py
+++ sqldb.py
@@ -9,7 +9,7 @@ database = 'CoronaVirus'
 username = 'logecal'
 password = 'Lushan2020$'
 
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 st.title('Surgery Time table and plots')
 
