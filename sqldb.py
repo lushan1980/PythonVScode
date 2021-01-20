7113c0bf81c182c66b228cc1f0232962d61aa69b
lushan1980 <shanswork@yahoo.com>
2021-01-20 07:49:08 -0500

    Update sqldb.py

--- sqldb.py
+++ sqldb.py
@@ -10,7 +10,7 @@ username = 'logecal'
 password = 'Lushan2020$'
 
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+conn = pyodbc.connect('DRIVER={FreeTDS};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 
 st.title('Surgery Time table and plots')
